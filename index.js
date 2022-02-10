7a0fe4755556ad4f3ce13cbc7b1d4619b48c984f
raccoonAndy <byhachi11@gmail.com>
2022-02-10 12:59:15 +0300

    learn express / response methods

--- index.js
+++ index.js
@@ -3,29 +3,20 @@ import express from 'express';
 const port = 8080;
 const app = express();
 
-// It's like middleware between send and handler request
-// you can use it for handle errors, for example
-// next() — send request next handler (not required)
-app.all('/hello', (req, res, next) => {
-  console.log("I'm before get handlers");
-  next();
-});
-
-// extra middleware
-const callback = (req, res, next) => {
-  console.log("I'm between all and get handlers");
-  next(); // go to next request
-};
-
-// type route:
-// '/hel?lo' — you can route on /hello or /helo
-// '/hel+lo' — unlimited numbers `l` after '+'
-// '/hel*lo' — unlimited numbers any symbols after '*'
-// '/he(la)?lo' — group symbols (/helo, /helalo), like regexp
-// '/.*a$/' — you ccan use regexp as well
-// you can add extra callbacks (array or one)
-app.get('/hello', callback, (req, res) => {
-  res.send('Hello, world!');
+// `res.send({ success: true })` — you can send json
+// `res.status(201).send({ success: true })` — you can send status
+// `res.status(201).json({ success: true })` — you can send json use method json
+// `res.download('/test.pdf', 'custom_name.pdf')` — download file
+// `res.redirect(301, 'http://other.som')` — redirect to other route
+// `res.set('Content-Type', 'text/plain')` — set http headers
+// `res.append('Content-Type', 'text/plain')` — add http headers
+// `res.type('text/plain')` — force set Content-Type to other type
+// `res.location() / res.link` — work with href and links
+// `res.cookie('token', 'value', {domain, path, secure, expires})` — work with cookie
+// `res.clearCookie('token', {path})` — clear cookie by name
+// `res.end()` — finish request (import)
+app.get('/hello', (req, res) => {
+  res.send({ success: true });
 });
 
 app.listen(port, () => {
