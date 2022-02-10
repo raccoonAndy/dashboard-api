5580bc9e4ef2ede1155ccda30aa181da6fb69965
raccoonAndy <byhachi11@gmail.com>
2022-02-10 15:37:52 +0300

    learn express / handle errors

--- index.js
+++ index.js
@@ -4,12 +4,31 @@ import { userRouter } from './users/users.js';
 const port = 8080;
 const app = express();
 
+// Middleware steps
+// GET ->
+// Implement all global app.use() ->
+// Implement all sub routers (if consist) router.use() ->
+// Implement particular router.get('/route', [cb1, cb2, ...]) ->
+// Implement error handler app.use((err, ...)) ->
+// Return response
+
+// callback handler global router (for all app)
+app.use((req, res, next) => {
+  console.log(`Time enter: ${Date.now()}`);
+  next();
+});
+
 app.get('/hello', (req, res) => {
-  res.send({ success: true });
-  res.end();
+  throw new Error('Error!');
 });
 
 app.use('/users', userRouter);
+
+// ! handler error must be at the end after all .use()
+app.use((error, req, res, next) => {
+  console.log(error.message);
+  res.status(401).send(error.message);
+});
 
 app.listen(port, () => {
   console.log(`Server is started on http://localhost:${port}`);
--- users/users.js
+++ users/users.js
@@ -2,6 +2,12 @@ import express from 'express';
 
 const userRouter = express.Router();
 
+// callback handler for particular route
+userRouter.use((req, res, next) => {
+  console.log('Handle user router');
+  next();
+});
+
 userRouter.get('/login', (req, res) => {
   res.send('login');
 });
